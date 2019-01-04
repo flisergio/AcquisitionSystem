ccb45977fba8eede3890bad9ea5e7db7cfc945f4
Sergio <sergey.dko@gmail.com>
2019-01-04 13:33:52 +0100

    Fixed bugs with web page

--- venv/templates/chart.js
+++ venv/templates/chart.js
@@ -4,8 +4,8 @@ function mainHeader() {
     .style('font-size', '50px')
     .style('font-family', 'Georgia')
     .style('text-align', 'center')
-    .style('text-decoration', 'overline underline')
-    .style('text-decoration-style', 'dashed');
+    .style('text-decoration', 'underline')
+    .style('text-decoration-style', 'solid');
 }
 /*
 function appendBody(t) {
@@ -94,172 +94,199 @@ function myFunction() {
     var LabY;
     var LabX;
 
+    var inputText =  document.getElementById("myText");
+    inputText.value = "";
+    inputText.placeholder = "Provide spool ID";
+    if(inputText.classList.contains('input-placeholder-red')) {
+        inputText.classList.replace('input-placeholder-red', 'input-placeholder');
+    }
+    else {
+        inputText.classList.add('input-placeholder');
+    }
+
     createButtons();
 
     xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var my_data = JSON.parse(xmlhttp.responseText);
+        try {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var my_data = JSON.parse(xmlhttp.responseText);
 
-        LabX = my_data.labelY;
-        LabY = my_data.labelX;
+                LabX = my_data.labelY;
+                LabY = my_data.labelX;
 
-        var i;
-        var memY1, memY2, memA;
-        var j;
-        for (j = 0; j < 1000; j++) {
-            for (i = 0; i < (LabY.length - 1); i++) {
-                memY1 = LabY[i];
-                memY2 = LabY[i+1];
-                memA = (memY1 + memY2)/2.0;
-                LabY[i] = memY1 - (memY1 - memA)/2.0;
-                LabY[i+1] = memY2 - (memY2 - memA)/2.0;
+                var i;
+                var memY1, memY2, memA;
+                var j;
+                for (j = 0; j < 1000; j++) {
+                    for (i = 0; i < (LabY.length - 1); i++) {
+                        memY1 = LabY[i];
+                        memY2 = LabY[i+1];
+                        memA = (memY1 + memY2)/2.0;
+                        LabY[i] = memY1 - (memY1 - memA)/2.0;
+                        LabY[i+1] = memY2 - (memY2 - memA)/2.0;
+                    }
+                }
+
+                areaMin = my_data.diameter - 0.02;
+                areaMax = my_data.diameter + 0.02;
+
+                var xm = (LabX.length / 10) + (LabX.length / 1000);
+            }
+            /*
+            textDiameter = 'Diameter: ' + my_data.diameter;
+            textMaterial = 'Material: ' + my_data.material;
+            textColor = 'Color: ' + my_data.color;
+            textMean = 'Mean diameter: ' + my_data.mean;
+            textOvality = 'Ovality: ' + my_data.ovality;
+            textDeviation = 'Standard deviation: ' + my_data.deviation;
+            textDate = 'Date of production: ' + my_data.dateprod;
+
+            appendBody(textDiameter);
+            appendBody(textMaterial);
+            appendBody(textColor);
+            appendBody(textMean);
+            appendBody(textOvality);
+            appendBody(textDeviation);
+            appendBody(textDate);
+            */
+
+            var classParagraph = document.createElement("p");
+
+            var body = document.querySelector("body");
+
+            var classDiameterParagraph = document.createTextNode(my_data.diameter + " milimeters");
+            classParagraph.appendChild(classDiameterParagraph);
+            var classDiameter = document.getElementById("Diameter");
+            classDiameter.appendChild(classDiameterParagraph);
+
+            var classMaterialParagraph = document.createTextNode(my_data.material);
+            classParagraph.appendChild(classMaterialParagraph);
+            var classMaterial = document.getElementById("Material");
+            classMaterial.appendChild(classMaterialParagraph);
+
+            var classColorParagraph = document.createTextNode(my_data.color);
+            classParagraph.appendChild(classColorParagraph);
+            var classColor = document.getElementById("Color");
+            classColor.appendChild(classColorParagraph);
+
+            var classMeanParagraph = document.createTextNode(my_data.mean + " milimeters");
+            classParagraph.appendChild(classMeanParagraph);
+            var classMean = document.getElementById("Mean");
+            classMean.appendChild(classMeanParagraph);
+
+            var classOvalityParagraph = document.createTextNode(my_data.ovality);
+            classParagraph.appendChild(classOvalityParagraph);
+            var classOvality = document.getElementById("Ovality");
+            classOvality.appendChild(classOvalityParagraph);
+
+            var classDeviationParagraph = document.createTextNode(my_data.deviation);
+            classParagraph.appendChild(classDeviationParagraph);
+            var classDeviation = document.getElementById("Deviation");
+            classDeviation.appendChild(classDeviationParagraph);
+
+            var classDateParagraph = document.createTextNode(my_data.dateprod);
+            classParagraph.appendChild(classDateParagraph);
+            var classDate = document.getElementById("Date");
+            classDate.appendChild(classDateParagraph);
+
+            document.getElementById("defaultOpen").click();
+
+            var margin = {top: 20, right: 100, bottom: 30, left: 100},
+            width = 1500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+            rawData = LabX.map(function(d, i){
+              return { 'x': d, 'y' : LabY[i] };
+            }),
+            tempData = [
+                    { x: 0.3, y0: areaMax, y1: areaMin, },
+                    { x: xm, y0: areaMax, y1: areaMin, },
+            ];
+            data=[];
+            var svg = d3.select("body").append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                    .style("stroke", "black")
+                    .style("stroke-width", 1)
+                    .style("fill", "none");    
+                      
+            var scaleX = d3.scale.linear()
+                        .domain([0, xm])
+                        .range([0, width]);
+
+            if (my_data.diameter == 1.75) {           
+                var scaleY = d3.scale.linear()
+                            .domain([1.8, 1.7])
+                            .range([0, height]);
+            }
+            else if (my_data.diameter == 2.85) {
+                var scaleY = d3.scale.linear()
+                            .domain([2.95, 2.75])
+                            .range([0, height]);
+            }
+
+            for(i = 0; i < rawData.length; i++)
+                data.push({x: scaleX(rawData[i].x), y: scaleY(rawData[i].y)});
+                         
+            var xAxis = d3.svg.axis()
+                         .scale(scaleX)
+                         .orient("bottom").ticks(10)
+                         .innerTickSize(-height)
+                         .outerTickSize(0)
+                         .tickPadding(10);
+
+            var yAxis = d3.svg.axis()
+                         .scale(scaleY)
+                         .orient("left")
+                         .innerTickSize(-width)
+                         .outerTickSize(0)
+                         .tickPadding(10);
+                       
+            var area = d3.svg.area()
+                .x(function(d) { return scaleX(d.x); })
+                .y0(function(d) { return scaleY(d.y0); })
+                .y1(function(d) { return scaleY(d.y1); });
+
+            var line = d3.svg.line()
+                .x(function(d){return d.x;})
+                .y(function(d){return d.y;});
+
+            svg.append("g")      
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+             
+            svg.append("g") 
+                .attr("class", "y axis")
+                .call(yAxis);
+
+            svg.append("path")
+            .datum(tempData)
+            .attr("class", "area")
+            .attr("d", area);
+
+            svg.append("path")
+                .data([data])
+                .attr("class", "line")
+                .attr("d", line);
+        } catch(err) {
+            if (err.name == "SyntaxError") {
+                inputText.placeholder = "Please, provide valid spool ID";
+                if(inputText.classList.contains('input-placeholder')) {
+                    inputText.classList.replace('input-placeholder', 'input-placeholder-red');
+                }
+                else {
+                    inputText.classList.add('input-placeholder-red');
+                }
+
+//                var attributeButtons = document.getElementsByClassName("tablink");
+//                attributeButtons.parentNode.removeChild(attributeButtons);
+            }
+            else {
+                console.log(err);
             }
         }
-
-        areaMin = my_data.diameter - 0.02;
-        areaMax = my_data.diameter + 0.02;
-
-        var xm = (LabX.length / 10) + (LabX.length / 1000);
-    }
-    /*
-    textDiameter = 'Diameter: ' + my_data.diameter;
-    textMaterial = 'Material: ' + my_data.material;
-    textColor = 'Color: ' + my_data.color;
-    textMean = 'Mean diameter: ' + my_data.mean;
-    textOvality = 'Ovality: ' + my_data.ovality;
-    textDeviation = 'Standard deviation: ' + my_data.deviation;
-    textDate = 'Date of production: ' + my_data.dateprod;
-
-    appendBody(textDiameter);
-    appendBody(textMaterial);
-    appendBody(textColor);
-    appendBody(textMean);
-    appendBody(textOvality);
-    appendBody(textDeviation);
-    appendBody(textDate);
-    */
-
-    var classParagraph = document.createElement("p");
-
-    var body = document.querySelector("body");
-
-    var classDiameterParagraph = document.createTextNode(my_data.diameter + " milimeters");
-    classParagraph.appendChild(classDiameterParagraph);
-    var classDiameter = document.getElementById("Diameter");
-    classDiameter.appendChild(classDiameterParagraph);
-
-    var classMaterialParagraph = document.createTextNode(my_data.material);
-    classParagraph.appendChild(classMaterialParagraph);
-    var classMaterial = document.getElementById("Material");
-    classMaterial.appendChild(classMaterialParagraph);
-
-    var classColorParagraph = document.createTextNode(my_data.color);
-    classParagraph.appendChild(classColorParagraph);
-    var classColor = document.getElementById("Color");
-    classColor.appendChild(classColorParagraph);
-
-    var classMeanParagraph = document.createTextNode(my_data.mean + " milimeters");
-    classParagraph.appendChild(classMeanParagraph);
-    var classMean = document.getElementById("Mean");
-    classMean.appendChild(classMeanParagraph);
-
-    var classOvalityParagraph = document.createTextNode(my_data.ovality);
-    classParagraph.appendChild(classOvalityParagraph);
-    var classOvality = document.getElementById("Ovality");
-    classOvality.appendChild(classOvalityParagraph);
-
-    var classDeviationParagraph = document.createTextNode(my_data.deviation);
-    classParagraph.appendChild(classDeviationParagraph);
-    var classDeviation = document.getElementById("Deviation");
-    classDeviation.appendChild(classDeviationParagraph);
-
-    var classDateParagraph = document.createTextNode(my_data.dateprod);
-    classParagraph.appendChild(classDateParagraph);
-    var classDate = document.getElementById("Date");
-    classDate.appendChild(classDateParagraph);
-
-    document.getElementById("defaultOpen").click();
-
-    var margin = {top: 20, right: 100, bottom: 30, left: 100},
-    width = 1500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    rawData = LabX.map(function(d, i){
-      return { 'x': d, 'y' : LabY[i] };
-    }),
-    tempData = [
-            { x: 0.3, y0: areaMax, y1: areaMin, },
-            { x: xm, y0: areaMax, y1: areaMin, },
-    ];
-    data=[];
-
-    var svg = d3.select("body").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .style("stroke", "black")
-            .style("stroke-width", 1)
-            .style("fill", "none");    
-              
-    var scaleX = d3.scale.linear()
-                .domain([0, xm])
-                .range([0, width]);
-
-    if (my_data.diameter == 1.75) {           
-        var scaleY = d3.scale.linear()
-                    .domain([1.8, 1.7])
-                    .range([0, height]);
-    }
-    else if (my_data.diameter == 2.85) {
-        var scaleY = d3.scale.linear()
-                    .domain([2.95, 2.75])
-                    .range([0, height]);
-    }
-
-    for(i = 0; i < rawData.length; i++)
-        data.push({x: scaleX(rawData[i].x), y: scaleY(rawData[i].y)});
-                 
-    var xAxis = d3.svg.axis()
-                 .scale(scaleX)
-                 .orient("bottom").ticks(10)
-                 .innerTickSize(-height)
-                 .outerTickSize(0)
-                 .tickPadding(10);
-
-    var yAxis = d3.svg.axis()
-                 .scale(scaleY)
-                 .orient("left")
-                 .innerTickSize(-width)
-                 .outerTickSize(0)
-                 .tickPadding(10);
-               
-    var area = d3.svg.area()
-        .x(function(d) { return scaleX(d.x); })
-        .y0(function(d) { return scaleY(d.y0); })
-        .y1(function(d) { return scaleY(d.y1); });
-
-    var line = d3.svg.line()
-        .x(function(d){return d.x;})
-        .y(function(d){return d.y;});
-
-    svg.append("g")      
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-     
-    svg.append("g") 
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    svg.append("path")
-    .datum(tempData)
-    .attr("class", "area")
-    .attr("d", area);
-
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", line);
     };
 
     xmlhttp.open("GET", url, true);
